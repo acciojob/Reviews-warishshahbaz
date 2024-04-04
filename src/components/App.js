69c40d8d3949a244d2fe13b662a0550618c22d50
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-04 15:49:53 +0530

    implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,93 +1,23 @@
-{
-  /* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */
-}
-import React, { useState } from "react";
-import Review from "./review";
+import React from "react";
+import "./App.css";
+import Review from "./Review";
 
-export default function App() {
-  const [count, setCount] = useState(1);
+// function App(){
 
-  function renedomGenerator() {
-    let res = Math.floor(Math.random() * Data.length);
-    if (res === 0) {
-      res = 1;
-    }
+// }
 
-    setCount(res);
-  }
-
+const App = () => {
   return (
-    <>
-      <div className="main">
-        <h2 id="review-heading">Our Reviews</h2>
-        <main className="review">
-          {Data.slice(count - 1, count).map((val, i) => {
-            return <Review val={val} key={i} />;
-          })}
-        </main>
-      </div>
-      <main className="btn_box">
-        <div id="author-1" className="review">
-          <button
-            id="author-2"
-            className="prev-btn"
-            onClick={() => {
-              if (count > 1) {
-                setCount((pre) => pre - 1);
-              }
-            }}
-          >
-            prev
-          </button>
-          <button className="random-btn" onClick={renedomGenerator}>
-            surprise me
-          </button>
-          <button
-            id="author-4"
-            className="next-btn"
-            onClick={() => {
-              if (Data.length > count) setCount((pre) => pre + 1);
-            }}
-          >
-            Next
-          </button>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2 id="review-heading">Our Reviews</h2>
+          <div className="underline"></div>
         </div>
-      </main>
-    </>
+        <Review />
+      </section>
+    </main>
   );
-}
+};
 
-const Data = [
-  {
-    id: 1,
-    name: "susan smith",
-    job: "web developer",
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-  },
-  {
-    id: 2,
-    name: "anna johnson",
-    job: "web designer",
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
-    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-  },
-  {
-    id: 3,
-    name: "peter jones",
-    job: "intern",
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-  },
-  {
-    id: 4,
-    name: "bill anderson",
-    job: "the boss",
-    image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
-    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-  },
-];
+export default App;
--- src/components/review.js
+++ src/components/review.js
@@ -1,28 +1,108 @@
 import React from "react";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import people from "./data";
 
-function Review({ val }) {
-  return (
-    <div key={val.id} className="container">
-      <div className="box">
-        <p>Author</p>
-        <span className="author" id="author-id">
-          {val.name}
-        </span>
-      </div>
-      <div className="box">
-        <p>Job</p>
-        <span className="job">{val.job}</span>
-      </div>
-      <div className="box">
-        <p>Text</p>
-        <span className="info">{val.text}</span>
-      </div>
-      <div className="box">
-        <p>Image</p>
-        <img className="person-img" src={val.image} alt="img" />
-      </div>
-    </div>
-  );
+class Review extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+    };
+
+    this.randomPerson = this.randomPerson.bind(this);
+    this.prevPerson = this.prevPerson.bind(this);
+    this.nextPerson = this.nextPerson.bind(this);
+  }
+
+  prevPerson = () => {
+    let newIndex = this.state.index - 1;
+    if (newIndex < 0) {
+      newIndex = people.length - 1;
+    }
+    this.setState({ index: newIndex });
+  };
+
+  nextPerson = () => {
+    let newIndex = this.state.index + 1;
+    if (newIndex >= people.length) {
+      newIndex = 0;
+    }
+    this.setState({ index: newIndex });
+  };
+  randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === this.state.index) {
+      randomIndex += 1;
+    }
+    if (randomIndex >= people.length) {
+      randomIndex = 0;
+    }
+    this.setState({ index: randomIndex });
+  };
+  render() {
+    const { name, job, image, text } = people[this.state.index];
+
+    return (
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+
+        <div className="button-container">
+          <button className="prev-btn" onClick={this.prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={this.nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="random-btn" onClick={this.randomPerson}>
+          surprise me
+        </button>
+      </article>
+    );
+  }
 }
 
 export default Review;
+
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
